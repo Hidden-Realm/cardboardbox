287226bf7798cd67de633afc3d7a0d8b884cab36
Hidden-Realm <45520420+Hidden-Realm@users.noreply.github.com>
2020-03-09 15:38:43 -0700

    Add files via upload

---
+++ editor/ChangeNotifier.ts
@@ -0,0 +1,33 @@
+// Copyright (C) 2020 John Nesky, distributed under the MIT license.
+
+namespace beepbox {
+	export class ChangeNotifier {
+		private _watchers: (() => void)[] = [];
+		private _dirty: boolean = false;
+
+		public watch(watcher: () => void): void {
+			if (this._watchers.indexOf(watcher) == -1) {
+				this._watchers.push(watcher);
+			}
+		}
+
+		public unwatch(watcher: () => void): void {
+			const index: number = this._watchers.indexOf(watcher);
+			if (index != -1) {
+				this._watchers.splice(index, 1);
+			}
+		}
+
+		public changed(): void {
+			this._dirty = true;
+		}
+
+		public notifyWatchers(): void {
+			if (!this._dirty) return;
+			this._dirty = false;
+			for (const watcher of this._watchers.concat()) {
+				watcher();
+			}
+		}
+	}
+}
